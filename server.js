e1938770da68d0d2ec01256725ddd1814d3c41c7
DivySrivastava <dj.srivastava23@gmail.com>
2019-02-05 10:26:21 +0530

    realese-2 add nodeJS server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const fs = require('fs');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, './client')));
+
+const server = http.createServer(app);
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '2374';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`Server running on http:/localhost:${port}`));
